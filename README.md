1d61c0135c3452fd75a379fc2c4ea1444a7925e2
k mccall <kmccall@microsoft.com>
2020-01-03 18:35:19 -0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
 # udacity-unityMLagents-continuousControl
+udacity continuous control programming assignment learning to control the movement of a double jointed arm to track spheres in unity MLagents
+
+# Project Details 
+In this project, we trained a DRL agent with the Unity MLAgents environment to teach a robot how to control the movement of a double jointed arm in order to track the movement of spheres in three dimensional space.  It does this by affecting four torque values that control the joints. 
+
+Each action is a vector of these four floating point torque values, corresponding to the torque to be applied to the two joints. Every entry in the action vector should be a number between -1.0 and 1.0. The observation space consists of 33 variables corresponding to position, rotation, velocity, and angular velocities of the arm.
+
+A reward of +0.1 is provided for each step that the agent's hand is in the goal location. Thus, the goal of the agent is to maintain its position at the target location for as many time steps as possible.  The environment is considered ‘solved’ when the average score is above 30.0 for the trailing 100 consecutive episodes.
+
+The task is episodic. There were two possible projects possible, whereby a single agent is trained by itself, with the objective to achieve the minimum 30.0 score. Alternatively, a set of 20 agents could be trained at the same time, by paralleling the matrix computation required for calculations at every step of training. In that example, solving the environment requires achieving an average of 30.0 across all agents. 
+
+# Getting Started
+The project is comprised of three files:
+1. **continuous_control.ipynb**: imports packages, connects to Unity environment, and then runs the learning function that will interact with that environment, leveraging various neural networks defined in model.py and the Agent class defined in ddpg_agent.py
+2. **model.py**: contains the pytorch neural networks (two actor networks, and two critic networks), that actively learn and store the learnings from the DDPG training process  
+3. **ddpg_agent.py**: contains the code to initialize the (four) networks, a replay buffer class, an OUNoise class, and wrappers for selecting actions, submitting them to the environment (through 'step') and then learn from experience stored in the replay buffer
+
+Logically, there are three steps required in order to execute the project: Download the files referred to above, install the necessary Unity MLAgents environment, and execute the code in the environment of your choice, such as a Jupyter Notebook environment (which is recommended for simplicity).  Here are the more detailed instructions for doing so:
+1. Download the three files above (from the public github repo)   
+Execute this command (at a shell prompt, in the appropriate source repo location where you wish to copy the files): \
+$ *git clone https://github.com/kamccall/udacity-unityMLagents-continuousControl* \
+This will clone the entire repository - including the three source files as well as associated README and REPORT files - into that source directory. 
+1. Install the Unity MLAgents environment (within which the agent will train and subsequently execute)   
+Follow the directions below in order to install the needed 'Reacher' environment for Unity MLAgents: \
+https://github.com/udacity/deep-reinforcement-learning/tree/master/p2_continuous-control#getting-started
+1. Execute the code (either within a Jupyter Notebook environment, or within an alternative IDE) \
+There are dependencies upon various Python packages in order to successfully execute this project, such as numpy, pyplot, torch, and others. Depending upon your Python environment, it is likely that those packages are already installed, in which case the 'import' commands in the code will successfully execute, thereby allowing the use of the code in those packages.\
+If your environment does not already have all of these packages installed, it is possible that you will need to install one or more packages before you can successfully execute the 'import' command to utilize them.  If that is the case:
+    1. ADD a new cell at the top of your Jupyter notebook
+    1. Follow the appropriate directions in this link to install the required packages (which will be different depending upon whether you are using 'pip' or 'conda' as your package manager): https://jakevdp.github.io/blog/2017/12/05/installing-python-packages-from-jupyter/
+
+# Build and Test Instructions
+There are three ways to run the code:
+1. Install all three files in the same directory, and execute the navigation .ipynb notebook file in a Jupyter notebook server , or
+2. Insert the code from 'model.py' and 'dqn_agent.py' into new (inserted) cells in the .ipynb notebook file, and execute everything from the single notebook file within a Jupyter notebook environment, or
+3. (More difficult): Copy and paste the Python code from within the .ipynb notebook file into a Python source code file, and then execute the program from the command line (with all appropriate dependencies based on your IDE and OS)
+
--- ddpg_agent.py
+++ ddpg_agent.py
@@ -3,140 +3,107 @@ import random
 import copy
 from collections import namedtuple, deque
 
-from model import Actor, Critic
-
 import torch
+import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 
-BUFFER_SIZE = int(1e5)  # replay buffer size
+from model import Actor, Critic
+
+BUFFER_SIZE = int(1e6)  # replay buffer size
 BATCH_SIZE = 128        # minibatch size
 GAMMA = 0.99            # discount factor
 TAU = 1e-3              # for soft update of target parameters
 LR_ACTOR = 1e-4         # learning rate of the actor 
 LR_CRITIC = 1e-3        # learning rate of the critic
 WEIGHT_DECAY = .0001    # L2 weight decay
-UPDATE_EVERY = 4        # KM ADDED for network updates
 
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
 class Agent():
-    """Interacts with and learns from the environment."""
-    
-    def __init__(self, state_size, action_size, random_seed):
-        """Initialize an Agent object.
-        
-        Params
-        ======
-            state_size (int): dimension of each state
-            action_size (int): dimension of each action
-            random_seed (int): random seed
-        """
+    def __init__(self, n_agents, state_size, action_size, random_seed):      
+        self.n_agents = n_agents
         self.state_size = state_size
         self.action_size = action_size
-        self.seed = random.seed(random_seed)
-
-        # Actor Network (w/ Target Network)
+        
+        # actor networks local and target
         self.actor_local = Actor(state_size, action_size, random_seed).to(device)
         self.actor_target = Actor(state_size, action_size, random_seed).to(device)
-        self.actor_optimizer = optim.Adam(self.actor_local.parameters(), lr=LR_ACTOR) # associated with actor_local
-
-        # Critic Network (w/ Target Network)
+        self.actor_optimizer = optim.Adam(self.actor_local.parameters(), lr = LR_ACTOR)
+        
+        ## critic networks local and target
         self.critic_local = Critic(state_size, action_size, random_seed).to(device)
         self.critic_target = Critic(state_size, action_size, random_seed).to(device)
-        self.critic_optimizer = optim.Adam(self.critic_local.parameters(), lr=LR_CRITIC, weight_decay=WEIGHT_DECAY) # critic local
-
-        # Noise process
-        self.noise = OUNoise(action_size, random_seed)
-
-        # Replay memory
-        self.memory = ReplayBuffer(action_size, BUFFER_SIZE, BATCH_SIZE, random_seed)
-        self.t_step = 0
-    
-    def step(self, state, action, reward, next_state, done):
-        """Save experience in replay memory, and use random sample from buffer to learn."""
-        # Save experience / reward
-        self.memory.add(state, action, reward, next_state, done)
-        self.t_step += 1
+        self.critic_optimizer = optim.Adam(self.critic_local.parameters(), lr = LR_CRITIC)
         
-        # Learn, if enough samples are available in memory
-        if self.t_step % UPDATE_EVERY == 0:
-            if len(self.memory) > BATCH_SIZE:
-                experiences = self.memory.sample()
-                self.learn(experiences, GAMMA)
-
-    def act(self, state, add_noise=True):
-        """Returns actions for given state as per current policy."""
-        state = torch.from_numpy(state).float().to(device)
-        self.actor_local.eval()   # picks action based on actor_local network (but calcs value on actor_target below)
+        # create noise and replaybuffer objects (based on classes defined below)
+        self.noise = OUNoise(action_size, random_seed)
+        self.memory = ReplayBuffer(action_size, BUFFER_SIZE, BATCH_SIZE, random_seed)
+    
+    def act(self,state, add_noise = True):
+        state = torch.from_numpy(state).float().to(device) # convert state from numpy array to a tensor
+        
+        self.actor_local.eval() # put network in evaluation mode, no trainig
+        
         with torch.no_grad():
             action = self.actor_local(state).cpu().data.numpy()
+            
         self.actor_local.train()
+        
         if add_noise:
             action += self.noise.sample()
-        return np.clip(action, -1, 1)
+            
+        return np.clip(action, -1, 1)  # ensure action space is in valid range of -1.0 to 1.0
         
-    def reset(self):
-        self.noise.reset()
-
-    def learn(self, experiences, gamma):
-        """Update policy and value parameters using given batch of experience tuples.
-        Q_targets = r + γ * critic_target(next_state, actor_target(next_state))
-        where:
-            actor_target(state) -> action
-            critic_target(state, action) -> Q-value
-
-        Params
-        ======
-            experiences (Tuple[torch.Tensor]): tuple of (s, a, r, s', done) tuples 
-            gamma (float): discount factor
-        """
-        states, actions, rewards, next_states, dones = experiences
-
-        # ---------------------------- update critic ---------------------------- #
-        # Get predicted next-state actions and Q values from target models
+    def step(self, state, action, reward, next_state, done): 
+        for i in range(self.n_agents):                             # used for 20 agent run
+            self.memory.add(state[i,:], action[i,:], reward[i], next_state[i,:], done[i])
+            
+        # self.memory.add(state, action, reward, next_state, done) # used for single agent run
+        
+        if len(self.memory) > BATCH_SIZE:
+            experiences = self.memory.sample()
+            self.learn(experiences, GAMMA)
+    
+    def learn(self, experiences, gamma):  
+        # obtain all the necessary contents from the sampled replay buffer
+        states, actions, rewards, next_states, dones = experiences        
+        
+        # get Q_target for next_states to compute Q_target for current state
         actions_next = self.actor_target(next_states)
         Q_targets_next = self.critic_target(next_states, actions_next)
-        # Compute Q targets for current states (y_i)
-        Q_targets = rewards + (gamma * Q_targets_next * (1 - dones))
+        Q_targets = rewards + (gamma * Q_targets_next * (1-dones))
         
-        # compute and minimize critic loss
+        # compute the actual Q_target based on current critic_local network
         Q_expected = self.critic_local(states, actions)
-        critic_loss = F.mse_loss(Q_expected, Q_targets)
-        
+        Q_loss = F.mse_loss(Q_expected, Q_targets)
         self.critic_optimizer.zero_grad()
-        critic_loss.backward()
+        Q_loss.backward()
+        torch.nn.utils.clip_grad_norm_(self.critic_local.parameters(), 1)  # KM important for stable training
         self.critic_optimizer.step()
-
-        # ---------------------------- update actor ---------------------------- #
-        # compute and minimize actor loss
-        actions_pred = self.actor_local(states)
-        actor_loss = -self.critic_local(states, actions_pred).mean()
         
+        # train actor_local to maximize Q
+        actions_pred = self.actor_local(states)
+        Q_value = - self.critic_local(states, actions_pred).mean()
+    
         self.actor_optimizer.zero_grad()
-        actor_loss.backward()
+        Q_value.backward()
         self.actor_optimizer.step()
-
-        # ----------------------- update target networks ----------------------- #
-        self.soft_update(self.critic_local, self.critic_target, TAU)
-        self.soft_update(self.actor_local, self.actor_target, TAU)                     
-
+        
+        # soft update target params
+        self.soft_update( self.actor_local, self.actor_target, TAU)
+        self.soft_update( self.critic_local, self.critic_target, TAU)       
+    
     def soft_update(self, local_model, target_model, tau):
-        """Soft update model parameters.
-        θ_target = τ*θ_local + (1 - τ)*θ_target
-
-        Params
-        ======
-            local_model: PyTorch model (weights will be copied from)
-            target_model: PyTorch model (weights will be copied to)
-            tau (float): interpolation parameter 
-        """
-        for target_param, local_param in zip(target_model.parameters(), local_model.parameters()):
-            target_param.data.copy_(tau*local_param.data + (1.0-tau)*target_param.data)
-
+        # periodically move values between local and target networks
+        for target_params, local_params in zip(target_model.parameters(), local_model.parameters()):
+            target_params.data.copy_( tau * local_params.data + (1-tau) * target_params.data)
+    
+    def reset(self):
+        self.noise.reset()
+    
 class OUNoise:
     """Ornstein-Uhlenbeck process."""
-
     def __init__(self, size, seed, mu=0., theta=0.15, sigma=0.05):
         """Initialize parameters and noise process."""
         self.mu = mu * np.ones(size)
@@ -158,7 +125,6 @@ class OUNoise:
 
 class ReplayBuffer:
     """Fixed-size buffer to store experience tuples."""
-
     def __init__(self, action_size, buffer_size, batch_size, seed):
         """Initialize a ReplayBuffer object.
         Params
